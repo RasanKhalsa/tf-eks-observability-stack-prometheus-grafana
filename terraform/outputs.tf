a7d15acabfefa1e5f96987aff59688e2d312313f
Rasanpreet Singh <rasankhalsa@gmail.com>
2025-12-02 00:05:38 -0700

    updated outputs.tf
    Removed commented-out access instructions for services.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,32 +23,3 @@ output "alertmanager_service" {
   description = "Alertmanager service name"
   value       = "kube-prometheus-stack-alertmanager.${kubernetes_namespace.monitoring.metadata[0].name}.svc.cluster.local:9093"
 }
-/*
-output "access_instructions" {
-  description = "Instructions to access services"
-  value       = <<-EOT
-    
-    === Access Instructions ===
-    
-    1. Grafana:
-       %{if var.enable_grafana_loadbalancer~}
-       - Get LoadBalancer URL:
-         kubectl get svc -n ${kubernetes_namespace.monitoring.metadata[0].name} kube-prometheus-stack-grafana
-       %{else~}
-       - Port forward:
-         kubectl port-forward -n ${kubernetes_namespace.monitoring.metadata[0].name} svc/kube-prometheus-stack-grafana 3000:80
-       %{endif~}
-       - Username: admin
-       - Password: ${var.grafana_admin_password}
-    
-    2. Prometheus:
-       - Port forward:
-         kubectl port-forward -n ${kubernetes_namespace.monitoring.metadata[0].name} svc/kube-prometheus-stack-prometheus 9090:9090
-       - Access: http://localhost:9090
-    
-    3. Alertmanager:
-       - Port forward:
-         kubectl port-forward -n ${kubernetes_namespace.monitoring.metadata[0].name} svc/kube-prometheus-stack-alertmanager 9093:9093
-       - Access: http://localhost:9093
-  EOT
-}*/
